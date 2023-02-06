91fc9d52e72ab9939dfc6b16a427fc0c63c81267
Rion Mia <mdrijonmia1271@gmail.com>
2023-02-06 12:27:36 +0600

    Update & Complete

---
+++ deashBoard.php
@@ -0,0 +1,214 @@
+
+<?php
+
+// $connect = new mysqli('localhost','root','','simple_ecommerce');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=simple_ecommerce','root','');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $pdo->prepare('SELECT * FROM deash_board ORDER BY id DESC');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Admin Panel</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:wght@400;500;700;900&family=Rubik:wght@400;500;600;700&display=swap"
+    rel="stylesheet" />
+  <link rel="stylesheet" href="./css/bootstrap.min.css" />
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.3/font/bootstrap-icons.min.css" />
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="./css/responsive.css" />
+</head>
+
+<body>
+  <!-- sidebar menu start -->
+  <div class="page-container">
+    <div class="sidebar-menu"> 
+      <div class="sidebar-header">
+        <div class="logo">
+          <a href="index.html"> <img src="./img/290018488_1110770999821005_2691879510461168954_n.png" alt=""></a>
+        </div>
+      </div>
+      <div class="header-text">
+        <a href="deashBoard.php"><i class="bi bi-house-door"></i>Dashboard</a>
+      </div>
+      <div class="main-menu">
+        <div class="menu-inner">
+          <nav>
+            <ul class="metismenu" id="menu">
+              <li class="management">Management</li>
+              <li>
+                <a href="productCategory.php"  
+                 ><i class="bi bi-graph-up"></i><span>Products Category </span>
+                </a>
+
+                <ul class="collapse show" id="services">
+                  <li><a href="ProductSubCategory.php">Product Sub Category</a></li>
+                  <li><a href="productColor.php">Product Colors</a></li>
+                  <li><a href="productSize.php">Product Sizes</a></li>
+                  <li><a href="productPacket.php">Product Packet</a></li>
+                </ul>
+              </li>
+              <li><a href="user.php" data-bs-toggle="collapse show"aria-expanded="true"><i
+                    class="bi bi-graph-up"></i><span>Registration </span>
+                </a>
+              </li>
+              <li><a href="login.php" data-bs-toggle="collapse show"aria-expanded="true"><i
+                    class="bi bi-graph-up"></i><span>Log Out </span>
+                </a>
+              </li>         
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <!-- sidebar menu End -->
+
+    <!-- header area start -->
+    <div class="main-content">
+      <div class="header-area sticky-top ">
+        <div class="row align-items-center">
+          <div class="col-md-2 col-sm-2 header-left">
+            <div class="nav-btn pull-left">
+              <span><i class="bi bi-list"></i></span>
+            </div>
+          </div>
+         
+        </div>
+      </div>
+      <!-- header area end -->
+
+      <!-- main content area start -->
+      <div class="main-content-inner pt-4">
+        <div class="row pb-3 pt-3">
+          <div class="col-md-3 col-sm-3">
+            <h4 style="margin-bottom: 0;">DeshBoard</h4>
+          </div>
+          <div class="col-md-10 col-sm-10"> </div>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-md-12">
+            <div class="recentUser">
+              <div class="card shadow">
+                <div class="card-body">
+                  <div class="row pt-4 pb-4 px-2">
+                    <div class="col-md-12">
+                      <div class="product-add-btn">
+                        <a  href="addDeashBoard.php" class="add-user">Add Product</a>
+                       </div>
+                      <!-- This is table data show start -->
+                       <table class="table"">
+                        <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Admin Name</th>
+                            <th scope="col">Category Name</th>
+                            <th scope="col">Sub Category Name</th>
+                            <th scope="col">Color Name</th>
+                            <th scope="col">Size Name</th>
+                            <th scope="col">Packet Name</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Create Date</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody> 
+                        <?php
+                        foreach ($products as $i => $product): ?>
+                            <tr>
+                                <th scope="row"><?php echo $i+1 ?></th>
+                                <td><?php echo $product ['admin_name'] ?></td>
+                              
+                                <td><?php
+                                  $product_cate_name_id = $product ['category_name_id'];
+                                  $con = mysqli_connect('localhost','root','','simple_ecommerce');
+                                  if ($result = mysqli_query($con, "SELECT * FROM category WHERE id = '$product_cate_name_id'")) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                      echo $row['category_name'];
+                                    }
+                                  }
+                                 ?></td>
+                                 <td><?php
+                                  $product_sub_cate_name_id = $product ['sub_category_name_id'];
+                                  $con = mysqli_connect('localhost','root','','simple_ecommerce');
+                                  if ($result = mysqli_query($con, "SELECT * FROM sub_category WHERE id = '$product_sub_cate_name_id'")) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                      echo $row['sub_category_name'];
+                                    }
+                                  }
+                                 ?></td>
+                                 <td><?php
+                                  $product_color_name_id = $product ['color_name_id'];
+                                  $con = mysqli_connect('localhost','root','','simple_ecommerce');
+                                  if ($result = mysqli_query($con, "SELECT * FROM colors WHERE id = '$product_color_name_id'")) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                      echo $row['color_name'];
+                                    }
+                                  }
+                                 ?></td>
+                                  <td><?php
+                                  $product_size_name_id = $product ['size_name_id'];
+                                  $con = mysqli_connect('localhost','root','','simple_ecommerce');
+                                  if ($result = mysqli_query($con, "SELECT * FROM sizes WHERE id = '$product_size_name_id'")) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                      echo $row['size_name'];
+                                    }
+                                  }
+                                 ?></td>
+                                 <td><?php
+                                  $product_pkt_name_id = $product ['packet_name_id'];
+                                  $con = mysqli_connect('localhost','root','','simple_ecommerce');
+                                  if ($result = mysqli_query($con, "SELECT * FROM packets WHERE id = '$product_pkt_name_id'")) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                      echo $row['packet_name'];
+                                    }
+                                  }
+                                 ?></td>
+                                 <td><?php echo $product ['quantity'] ?></td>
+                                <td><?php echo $product ['create_date'] ?></td>
+                                <td>
+                                    <a href="updateDeashBoard.php?id=<?php echo $product['id'] ?>" type="button" class="btn btn-sm btn-outline-primary">Edit</a>
+                                   <form style="display: inline-block" method="post" action="deleteDeshBoard.php" >
+                                       <input type="hidden" name="id" value="<?php echo $product['id'] ?>">
+                                       <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                                   </form>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                    <!-- This is table data show start -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <script src="./js/jquery-2.2.4.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script src="./js/app.js"></script>
+</body>
+
+</html>
---
+++ updatePacket.php
@@ -0,0 +1,189 @@
+
+<?php
+
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=simple_ecommerce','root','');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$id = $_GET['id'] ?? null;
+if (!$id){
+    header('Location: productPacket.php');
+    exit();
+}
+$statement = $pdo->prepare('SELECT * FROM packets WHERE id = :id');
+$statement->bindValue(':id', $id);
+$statement->execute();
+$product = $statement->fetch(PDO::FETCH_ASSOC);
+
+
+
+$empt_packet_name = '';
+$packet_name = $product['packet_name'];
+$packet_description = $product['packet_description'];
+if(isset($_POST['submit'])){
+  $packet_name = $_POST['packet_name'];
+  $packet_description = $_POST['packet_description'];
+  $status = isset($_POST['status']) && $_POST['status']  ? "1" : "0";
+  $date = date('Y-m-d H:i:s');
+    // var_dump($status);
+if(empty($packet_name)){
+  $empt_packet_name = 'Fill up this field';
+
+}
+if(!empty($packet_name)){
+    $statement = $pdo->prepare("UPDATE packets  SET packet_name = :packet_name, packet_description = :packet_description, status = :status
+    WHERE id = :id");
+        $statement->bindVAlue(':packet_name', $packet_name);
+        $statement->bindVAlue(':packet_description', $packet_description);
+        $statement->bindVAlue(':status', $status);
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+  header('location: productPacket.php');
+}
+
+}
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Admin Panel</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:wght@400;500;700;900&family=Rubik:wght@400;500;600;700&display=swap"
+    rel="stylesheet" />
+  <link rel="stylesheet" href="./css/bootstrap.min.css" />
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.3/font/bootstrap-icons.min.css" />
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="./css/responsive.css" />
+</head>
+
+<body>
+  <!-- sidebar menu start -->
+  <div class="page-container">
+    <div class="sidebar-menu">
+      <div class="sidebar-header">
+        <div class="logo">
+          <a href="index.html"> <img src="./img/290018488_1110770999821005_2691879510461168954_n.png" alt=""></a>
+        </div>
+      </div>
+      <div class="header-text">
+        <a href="deashBoard.php"><i class="bi bi-house-door"></i>Dashboard</a>
+      </div>
+      <div class="main-menu">
+        <div class="menu-inner">
+          <nav>
+            <ul class="metismenu" id="menu">
+              <li class="management">Management</li>
+              <li>
+                <a href="productCategory.php" data-bs-toggle="collapse show" data-bs-target="#productCategory" aria-controls="productCategory" aria-expanded="true"><i
+                    class="bi bi-graph-up"></i><span>Products Category </span>
+                </a>
+                <ul class="collapse show" id="services">
+                  <li><a href="ProductSubCategory.php">Product Sub Category</a></li>
+                  <li><a href="productColor.php">Product Colors</a></li>
+                  <li><a href="productSize.php">Product Sizes</a></li>
+                  <li><a href="productPacket.php">Product Packet</a></li>
+                </ul>
+              </li>
+              <li><a href="user.php" data-bs-toggle="collapse show"aria-expanded="true"><i
+                    class="bi bi-graph-up"></i><span>Registration </span>
+                </a>
+              </li>
+              <li><a href="login.php" data-bs-toggle="collapse show"aria-expanded="true"><i
+                    class="bi bi-graph-up"></i><span>Log Out </span>
+                </a>
+              </li> 
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <!-- sidebar menu End -->
+
+    <!-- header area start -->
+    <div class="main-content">
+      <div class="header-area sticky-top ">
+        <div class="row align-items-center">
+          <div class="col-md-2 col-sm-2 header-left">
+            <div class="nav-btn pull-left">
+              <span><i class="bi bi-list"></i></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- header area end -->
+
+      <!-- main content area start -->
+      <div class="main-content-inner pt-4">
+        <div class="row" >
+          <div class="col-md-12  " >
+            <div style="overflow-x:auto;" data-aos="fade-up" class="shadow bg-white  py-5 px-4">
+
+           
+
+           <form action="" method="post" enctype="multipart/form-data">
+              <h5> Update Products Packet</h5>
+              <table style="width:100%" class="current-data-table" > 
+                <tr>
+                  <th>Packet Name:</th>
+                  <td>
+                    <!-- <br> -->
+                  <input type="text" name="packet_name"  placeholder="Name" value="<?php echo $product['packet_name'] ?>">
+                  <br>
+                  <?php if(isset($_POST['submit'])){echo "<span class ='text-danger'>".$empt_packet_name."</span>";} ?>
+                </td>
+                </tr>
+                <br>
+                <tr>
+                  <th>Size Description:</th>
+                  <td>
+                      <br>
+                    <textarea cols="50" name="packet_description" placeholder="Description"><?php echo $product['packet_description'] ?></textarea>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Status:</th>
+                  <td><label class="switch">
+                      <input type="checkbox" name="status">
+                      <span class="slider round"></span>
+                    </label></td>
+                    <!-- <input type="checkbox" name="status" value="status" > -->
+                </tr> 
+              </table>
+              <!-- <div class="col-6 categorySubmit "> -->
+              <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                
+              <!-- </div> -->
+
+           </form>
+            
+          </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+
+  <script src="./js/jquery-2.2.4.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script src="./js/app.js"></script>
+
+</body>
+</html>
